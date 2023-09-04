5ff8348b0b3970af98a6112d6ace7d88a428aaf4
Mark Artishuk <markyshuk@gmail.com>
2023-09-04 15:52:58 -0700

    Go time

--- src/telegram/realTalkBot.ts
+++ src/telegram/realTalkBot.ts
@@ -2,10 +2,7 @@ import TelegramBot from 'node-telegram-bot-api';
 
 export const webhookUrl = process.env.WEBHOOK_URL;
 
-const realTalkBot = new TelegramBot(
-  process.env.TELEGRAM_API_TOKEN as string,
-  {}
-);
+const realTalkBot = new TelegramBot(process.env.TELEGRAM_API_KEY as string, {});
 
 realTalkBot.on('message', (message, metadata) => {
   console.log('MESSAGE: ', message.text, JSON.stringify(message));
