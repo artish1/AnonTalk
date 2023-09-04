9dd5d1c3b6429153363d1e7b20314508c3c04fac
Mark Artishuk <markyshuk@gmail.com>
2023-09-04 15:02:03 -0700

    GET also

--- src/app/api/viber/setwebhook/route.ts
+++ src/app/api/viber/setwebhook/route.ts
@@ -5,3 +5,8 @@ export async function POST(request: Request) {
   const res = await realTalkBot.setWebhook(process.env.WEBHOOK_URL as string);
   return NextResponse.json({ res });
 }
+
+export async function GET(request: Request) {
+  const res = await realTalkBot.setWebhook(process.env.WEBHOOK_URL as string);
+  return NextResponse.json({ msg: 'LETS TRY' });
+}
